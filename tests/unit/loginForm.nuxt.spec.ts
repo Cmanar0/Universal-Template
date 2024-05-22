8da751b6a85fd1a554da84ce806114e56cc937dc
Marian Adamus <cmanar0@gmail.com>
2024-05-22 13:47:51 +0200

    Update test (delete consol logs)

--- tests/unit/loginForm.nuxt.spec.ts
+++ tests/unit/loginForm.nuxt.spec.ts
@@ -8,50 +8,40 @@ vi.mock('axios')
 
 describe('LoginForm', () => {
   it('renders the form correctly', () => {
-    console.log('Starting test: renders the form correctly')
     const wrapper = mount(LoginForm)
     expect(wrapper.get('input[placeholder="Email"]')).toBeTruthy()
     expect(wrapper.get('input[placeholder="Password"]')).toBeTruthy()
     expect(wrapper.get('button[type="submit"]')).toBeTruthy()
-    console.log('Finished test: renders the form correctly')
   })
 
   it('initializes with empty fields', () => {
-    console.log('Starting test: initializes with empty fields')
     const wrapper = mount(LoginForm)
     const emailInput = wrapper.get('input[placeholder="Email"]')
     const passwordInput = wrapper.get('input[placeholder="Password"]')
 
     expect(emailInput.element.value).toBe('')
     expect(passwordInput.element.value).toBe('')
-    console.log('Finished test: initializes with empty fields')
   })
 
   it('toggles password visibility', async () => {
-    console.log('Starting test: toggles password visibility')
     const wrapper = mount(LoginForm)
     const passwordInput = wrapper.get('input[placeholder="Password"]')
 
     // Check initial state
     expect(passwordInput.attributes('type')).toBe('password')
-    console.log('Initial state: password field type is password')
 
     // Toggle visibility to 'text'
     const showPasswordButton = wrapper.get('#hide-password-icon')
     await showPasswordButton.trigger('click')
     expect(passwordInput.attributes('type')).toBe('text')
-    console.log('After first toggle: password field type is text')
 
     // Toggle visibility back to 'password'
     const hidePasswordButton = wrapper.get('#show-password-icon')
     await hidePasswordButton.trigger('click')
     expect(passwordInput.attributes('type')).toBe('password')
-    console.log('After second toggle: password field type is password')
-    console.log('Finished test: toggles password visibility')
   })
 
   it('submits the form and calls API', async () => {
-    console.log('Starting test: submits the form and calls API')
     const wrapper = mount(LoginForm)
     const emailInput = wrapper.get('input[placeholder="Email"]')
     const passwordInput = wrapper.get('input[placeholder="Password"]')
@@ -81,14 +71,9 @@ describe('LoginForm', () => {
         }
       }
     )
-    console.log(
-      'API called with email: test@example.com and password: password123'
-    )
-    console.log('Finished test: submits the form and calls API')
   })
 
   it('handles API error correctly', async () => {
-    console.log('Starting test: handles API error correctly')
     const wrapper = mount(LoginForm)
     const emailInput = wrapper.get('input[placeholder="Email"]')
     const passwordInput = wrapper.get('input[placeholder="Password"]')
@@ -123,9 +108,5 @@ describe('LoginForm', () => {
         }
       }
     )
-    console.log(
-      'API called with email: test@example.com and password: wrongpassword'
-    )
-    console.log('Finished test: handles API error correctly')
   })
 })
